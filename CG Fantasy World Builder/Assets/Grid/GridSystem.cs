0b34d6ce3fbf913fb931e5122fcd10746d12a3d9
90felipe09 <felhipeshiraishi@gmail.com>
2021-06-04 12:41:40 -0300

    add: multiple grid floors system

---
+++ CG Fantasy World Builder/Assets/Grid/GridSystem.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSystem : MonoBehaviour
+{
+    [SerializeField] private int floorShowing = 0;
+    [SerializeField] private GameObject gridPrefab;
+    [SerializeField] private List<GameObject> gridFloors;
+    [SerializeField] private int floorsAmount = 3;
+
+    private static float floorHeight = 3.0f;
+
+    private void Start()
+    {
+        instantiateGrids();
+        showGrid(0);
+    }
+
+    private void Update()
+    {
+        handleFloorNavigation();
+    }
+
+    private void instantiateGrids()
+    {
+        for (int floorIndex = 0; floorIndex < floorsAmount; floorIndex++)
+        {
+            GameObject grid = Instantiate(gridPrefab, transform);
+            grid.transform.position += Vector3.up * floorHeight * floorIndex;
+            grid.GetComponent<GridView>().setGridFloor(floorIndex);
+            grid.SetActive(false);
+            gridFloors.Add(grid);
+        }
+    }
+
+    public void showGrid(int floorToShow)
+    {
+        for (int floorIndex = 0; floorIndex < floorsAmount; floorIndex++)
+        {
+            if (floorIndex > floorToShow)
+            {
+                gridFloors[floorIndex].SetActive(false);
+                gridFloors[floorIndex].GetComponent<GridView>().deactivateShader();
+            }
+            else
+            {
+                gridFloors[floorIndex].SetActive(true);
+                gridFloors[floorIndex].GetComponent<GridView>().deactivateShader();
+            }
+        }
+
+        gridFloors[floorToShow].GetComponent<GridView>().activateShader();
+
+        floorShowing = floorToShow;
+    }
+
+    private void handleFloorNavigation()
+    {
+        if (Input.GetKeyDown(KeyCode.Minus))
+        {
+            if (floorShowing > 0)
+            {
+                showGrid(floorShowing - 1);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Equals))
+        {
+            if (floorShowing < floorsAmount - 1)
+            {
+                showGrid(floorShowing + 1);
+            }
+        }
+    }
+}
--- CG Fantasy World Builder/Assets/Grid/GridView.cs
+++ CG Fantasy World Builder/Assets/Grid/GridView.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class GridView : MonoBehaviour
 {
     [SerializeField] private int gridSize;
+    [SerializeField] private int gridFloor;
     [SerializeField] private GameObject tilePrefab;
     [SerializeField] private List<GameObject> tiles;
+
+    [SerializeField] private Shader outlinedShader;
+    [SerializeField] private Shader defaultShader;
 
     private GridModel gridModel;
 
@@ -23,11 +27,32 @@ public class GridView : MonoBehaviour
 
     private void mountGrid()
     {
-        for (int tileIndex = 0; tileIndex < gridSize * gridSize; tileIndex++)
+        for (int tileIndex = 0; tileIndex < gridModel.layout.Count; tileIndex++)
         {
             GameObject newTile = Instantiate(tilePrefab, this.transform);
             newTile.transform.localPosition = gridModel.layout[tileIndex].getPosition();
             tiles.Add(newTile);
+        }
+    }
+
+    public void setGridFloor(int floor)
+    {
+        gridFloor = floor;
+    }
+
+    public void deactivateShader()
+    {
+        for (int tileIndex = 0; tileIndex < tiles.Count; tileIndex++)
+        {
+            tiles[tileIndex].GetComponent<Renderer>().material.shader = defaultShader;
+        }
+    }
+
+    public void activateShader()
+    {
+        for (int tileIndex = 0; tileIndex < tiles.Count; tileIndex++)
+        {
+            tiles[tileIndex].GetComponent<Renderer>().material.shader = outlinedShader;
         }
     }
 }
